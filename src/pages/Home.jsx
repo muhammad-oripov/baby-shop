ad6c31bf014106822bcdc780344a06f4e9a0631d
DilshodM <dilshodmurtazoyev5@gmail.com>
2022-09-09 10:32:34 +0500

    helmet

---
+++ src/pages/Home.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import {Helmet} from "react-helmet";
+
+
+export default function Home() {
+  return (
+    <>
+        <Helmet>
+            <link rel="icon" href="https://static.npmjs.com/attachments/ck3uweazy72ye8874y9kkxnx1-gak.png" />
+            <title>Home</title>
+        </Helmet>
+        <div>home</div>
+    </>
+  )
+}
