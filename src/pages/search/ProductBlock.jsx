2ebd07f599723db405ae093bf7e2088cb1dcfffb
ShukurovOzodbek <shukurov.ozodbek.jaloliddinovich.com>
2022-09-18 17:00:48 +0500

    fix: fixed pagination

--- src/pages/search/ProductBlock.jsx
+++ src/pages/search/ProductBlock.jsx
@@ -2,8 +2,12 @@ import { Box, Rating, Typography } from '@mui/material';
 import React from 'react'
 import SVGIcons from '../../Components/SVGIcons';
 
-const ProductBlock = ({ products }) => {
+const ProductBlock = ({ products, onPixel }) => {
     const [val, setVal] = React.useState(4);
+
+    if(products < 5){
+        onPixel('1000px')
+    }
 
     return (
         <>
